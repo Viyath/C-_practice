f8d40982332134ea61292dca0192cccbdca06a5c
vi <kvkanthony@gmail.com>
2018-06-13 18:27:34 +1000

    Using If conditions

--- hello_world/hello_world/Program.cs
+++ hello_world/hello_world/Program.cs
@@ -6,9 +6,31 @@ namespace hello_world
     {
         static void Main(string[] args)
         {
-            var myame = "viyath";
-            Console.WriteLine("Hello C# World!");
-            Console.WriteLine(myame);
+            Console.WriteLine("Enter the name");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Enter your date");
+            var date = Console.ReadLine();
+
+            Console.WriteLine("Enter your month");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("your name is {0}" , name);
+            Console.WriteLine(month);
+            Console.WriteLine(date);
+
+            if (month == "january")
+            {
+                Console.WriteLine("You were born in Duruthu");
+            }
+            if (month == "february")
+            {
+                Console.WriteLine("You were born in Nawam");
+            }
+            if (month == "March")
+            {
+                Console.WriteLine("You were born in Madin");
+            }
         }
     }
 }
