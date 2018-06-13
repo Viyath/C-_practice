e3e81a8e5cc284656770fb0aeda1c176a18505b5
vi <kvkanthony@gmail.com>
2018-06-13 13:42:36 +1000

    Displaying Hellow world and the variable value

---
+++ hello_world/hello_world/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myame = "viyath";
+            Console.WriteLine("Hello C# World!");
+            Console.WriteLine(myame);
+        }
+    }
+}
