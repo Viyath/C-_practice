cc01c523bcabc3d20fe0919fd9347926bd35c027
vi <kvkanthony@gmail.com>
2018-06-13 23:56:06 +1000

    Lists - a Dynamic Array which can take values

---
+++ list/list/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = new List<string>();
+            var age = new List<int>();
+            var condition = true;
+            while (condition)
+            {
+                Console.WriteLine("Please enter the name ");
+                name.Add(Console.ReadLine());
+
+                Console.WriteLine("Please enter the age ");
+                age.Add(int.Parse(Console.ReadLine()));
+
+                Console.WriteLine("Do you want to continue? y/n");
+                
+                if (Console.ReadLine() != "y")
+                {
+                    condition = false;
+                }
+
+            }
+
+        }
+    }
+}
