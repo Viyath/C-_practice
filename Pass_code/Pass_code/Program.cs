1c17c9b701cf77750db117d2e729edca23b6ca73
vi <kvkanthony@gmail.com>
2018-06-13 18:46:13 +1000

    Simple if condition exercise

---
+++ Pass_code/Pass_code/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pass_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the username...");
+            var username = Console.ReadLine();
+
+            Console.WriteLine("Please enter the user password...");
+            var passcode = Console.ReadLine();
+
+            if (username=="viyath")
+            {
+                Console.WriteLine("User is identified");
+            }
+            if (passcode=="123")
+            {
+                Console.WriteLine("Success password!");
+            }
+        }
+    }
+}
