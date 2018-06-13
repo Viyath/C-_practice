6ab5cbd8194703c6b05023c82bc0d686079babea
vi <kvkanthony@gmail.com>
2018-06-13 21:42:16 +1000

    using arrays to store data and printing them

---
+++ arrays/arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new int[5];
+            student[0] = 10;
+            student[1] = 101;
+            student[2] = 102;
+            student[3] = 103;
+            student[4] = 310;
+            Console.WriteLine(student[2]);
+        }
+    }
+}
